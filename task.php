7a3f9bfdb9a9a29c5410fc7d7af6e3ab53c579b8
Yashir Rishad <86326063+EngineerArafat@users.noreply.github.com>
2022-02-02 09:20:12 +0600

    Add files via upload

---
+++ task.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<body>
+ <h2> Registration Form</h2>
+ <form action="/action_page.php">
+ <table>
+ Frist Name: </td>
+ <input type="text" name="name" ><br>
+ Last Name: </td>
+ <input type="text" name="name"><br>
+ Age: </td>
+ <input type="number"><br>
+ Designation: </td>
+ <input type="radio" name="program">Junior Programmer
+<input type="radio" name="program">Senior Programmer
+<input type="radio" name="program">Project Lead<br>
+Preferred language: </td>
+<input type= "checkbox" name="prgoram">JAVA
+<input type= "checkbox" name="program">PHP
+<input type= "checkbox" name ="program">C++<br>
+E-mail:
+<input type= "text"><br>
+Password:
+<input type="text" placeholder="***"><br>
+
+<label for="myfile">Please choose a file:</label>
+  <input type="file" id="myfile" name="myfile"><br><br>
+<input type="Submit">
+<input type="Reset">
+</table>
+</form>
+</html>
+ </body>
